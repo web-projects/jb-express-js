67e005752c18e88da4e76a837b332decab82ea66
Jonnie Ringo <web.projects@yahoo.com>
2018-08-09 12:25:20 -0700

    Added simple routes

--- app/app.js
+++ app/app.js
@@ -1,15 +1,57 @@
 var utils = require('./utils');
 var express = require("express");
 //var http = require("http");
+var dataFile = require('./data/data.json');
 
+// Initialize APP
 var app = express();
 
+// Get port from ENV variable
+app.set('port', process.env.PORT || 3000);
+
+// Setup ROUTE
 app.get('/', function(req, res) {
-  res.send('<h1>Roux Academy Meetups</h1>');
+
+  var info = "";
+
+  res.send(`
+            <h1>Welcome</h1>
+            <p>Roux Academy Meetups put together artists from all walks of life</p>
+          `);
 });
 
-var server = app.listen(3000, function() {
-  console.log(utils.getTimeStamp() + " server started listening on port: 3000");
+app.get('/speakers', function(req, res) {
+
+  var info = "";
+
+  dataFile.speakers.forEach(function(item) {
+    info += `
+    <li>
+      <h2>${item.name}</h2>
+      <p>${item.summary}</p>
+    </li>
+    `;
+  });
+
+  res.send(`
+            <h1>Roux Academy Meetups</h1>
+            ${info}
+          `);
+});
+
+app.get('/speakers/:speakersid', function(req, res) {
+
+  var speaker = dataFile.speakers[req.params.speakersid];
+
+  res.send(`
+            <h1>${speaker.title}</h1>
+            <h2>with ${speaker.name}</h2>
+            <p>${speaker.summary}</p>
+          `);
+});
+
+var server = app.listen(app.get('port'), function() {
+  console.log(utils.getTimeStamp() + " server started listening on port: " + app.get('port'));
 })
 
 /*
